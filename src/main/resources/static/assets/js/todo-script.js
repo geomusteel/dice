470bb74638a0bbbfc66d16e7d192d0bfc50f2319
geomusteel <geomusteel@naver.com>
2023-02-24 20:50:25 +0900

    수정사항 반영

--- src/main/resources/static/assets/js/todo-script.js
+++ src/main/resources/static/assets/js/todo-script.js
@@ -1,8 +1,37 @@
+const keyEventCheck = function () {
+    let textarea = document.getElementById('todo-input');
+    textarea.addEventListener('keydown', event => keyCodeCheck(event), {once: true});
+};
 
-const keyCodeCheck = function () {
-    if (window.event === 13) {
-
+const keyCodeCheck = function (event) {
+    let key = event.key || event.keyCode;
+    const todoInput = document.querySelector('#todo-input');
+    if (todoInput.value !== '' && key === 'Enter' || key === 13) {
+        createTodo(todoInput);
     }
+};
 
-    const inputValue = document.querySelector('#todo-input').value;
+const createTodo = function (todoInput) {
+    const todoList = document.querySelector('#todo-list');
+    const newLi = document.createElement('li');
+    const newSpan = document.createElement('span');
+    const newBtn = document.createElement('button')
+
+    newBtn.addEventListener('click', () => {
+        newLi.classList.toggle('complete');
+    });
+
+    newLi.addEventListener('dblclick', () => {
+        newLi.remove();
+    });
+
+    newSpan.textContent = todoInput.value;
+    newLi.appendChild(newBtn);
+    newLi.appendChild(newSpan);
+    todoList.appendChild(newLi);
+    todoInput.value = '';
+};
+
+const deleteAll = function (){
+    const liList = document.querySelectorAll('li');
 }
